a8bd7ee3c3453590cbc5ca1a627d76bca6e8b7b6
brians-123 <brian_shor@hotmail.com>
2020-11-24 15:05:12 -0800

    works on button click to pull from local storage but I want it to work from load. I had to hard code the jquery text creation after trying numerous different ways to loop through it.

--- scripts/script.js
+++ scripts/script.js
@@ -15,7 +15,7 @@ for (var i = 9; i < 24; i++) {
   var rowTime = $("<div>").addClass(
     "text-right col-1 p-3 mr-2 text-nowrap border-top"
   );
-  // .attr("data-calendarTime", i);
+  // .attr("data-calendar-time", i);
   var rowtimeText = i + " AM";
   if (i == 12) {
     rowtimeText = i + " PM";
@@ -27,17 +27,14 @@ for (var i = 9; i < 24; i++) {
   rowTime.text(rowtimeText);
   var rowSchedule = $("<input>")
     .addClass("w-100 p-3")
-    .val("another calendar subject")
-    .attr("data-calendarTime", i);
+    // .val("another calendar subject")
+    .attr("data-calendar-text", i)
+    .data("calendar-text", i)
+    .attr("id", i);
 
   var rowSaveButtonContainer = $("<div>")
     .addClass("saveBtn w-auto p-4 bg-info rounded-right")
-    .attr("data-calendarTime", i);
-
-  // .click(saveInLocal(i, $('input[data-calendarTime=" + i + "]').val()));
-  // console.log(i, $('input[data-calendarTime="' + i + '"]').val());
-
-  // $('"data-calendarTime="9"', "");
+    .data("calendar-time", i);
 
   var saveButtonImage = $(`
     <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-calendar-check" fill="currentColor"
@@ -58,79 +55,83 @@ for (var i = 9; i < 24; i++) {
   }
   newRow.append(rowTime, rowSchedule, rowSaveButtonContainer);
   calendarContainer.append(newRow);
-  console.log(i, $('input[data-calendarTime="9"]').val());
 }
-
-//when the loop finishes executing, it adds 1 to i, making it 24
-//this isn't what I want.
-//I'm going to move it outside the loop to add via event delegation
-
-// $(".saveBtn").on("click", function () {
-// saveInLocal(i, $('input[data-calendarTime="9"]').val());
-// saveInLocal("asdf", "123");
-//
-
-// saveInLocal(i, $('input[data-calendarTime="' + i + '"]').val());
-// console.log(i);
-// });
-
-// $("#saveBtn").on("click",function(){
-//   saveInLocal(i, $('input[data-calendarTime="' + i + '"]').val());
-// })
-
-//save the calendar text to local storage
-//commenting this out to try to use event delegation instead
-// function saveInLocal(calendarPosition, meetingText) {
-//   console.log(calendarPosition, meetingText);
-//   localStorage.setItem(calendarPosition, meetingText);
-// }
 
 //create an array to store data for the calendar text and positions
 //so we can pull them back out later
 var calendarData = [];
 
-//pull the data back out of localStorage and replace the existing placeholders
-console.log(JSON.parse(localStorage.getItem("WorkDayScheduler")));
+//create an object for the calendar times and text
+var thisCalendarObj = {};
 
-//get data out of local storage and push back to each element
-//later, this should go into the loop where I'm building each element
-// var calendarObj = JSON.parse(localStorage.getItem("WorkDayScheduler"));
+if (JSON.parse(localStorage.getItem("WorkDayScheduler")) != null) {
+  thisCalendarObj = JSON.parse(localStorage.getItem("WorkDayScheduler"));
 
-//use event delegation to grab the proper data element's value
-function saveInLocal(calendarPosition, meetingText) {
-  console.log($(this).data("calendartime"));
-  console.log($(this).parent().children("input").val());
-
-  //save the data element for the button's time into local storage
-  //along with the text of the input box next to it
-  // console.log($(this).parent().text());
-
-  var thisCalendarPosition = console.log($(this).data("calendartime"));
-  var thisCalendarText = $(this).parent().children("input").val();
-  var thisCalendarObj = { thisCalendarPosition: thisCalendarText };
-
-  //need to push into the object first to prevent duplicates
-
-  calendarData.push(thisCalendarObj);
-  console.log(calendarData);
-
-  localStorage.setItem("WorkDayScheduler", JSON.stringify(calendarData));
+  $.each(thisCalendarObj, function (key, value) {
+    if (value != null) {
+      // console.log(key);
+      // console.log(value);
+      // console.log(`[data-calendar-text="` + key + `"]`);
+      $("input")
+        .find("[data-calendar-text='" + key + "']")
+        .val(value);
+      $("input").find(`[data-calendar-text="9"]`).val("123321123");
+    }
+  });
 }
 
-$(function () {
-  $(".saveBtn").on("click", saveInLocal);
+$(".saveBtn").on("click", function saveInLocal() {
+  var thisCalendarPosition = $(this).data("calendar-time");
+
+  var thisCalendarText = $(this).parent().children("input").val();
+
+  thisCalendarObj[thisCalendarPosition] = thisCalendarText;
+
+  localStorage.setItem("WorkDayScheduler", JSON.stringify(thisCalendarObj));
+  //trying to add in button with possible order of execution issue
+  if (JSON.parse(localStorage.getItem("WorkDayScheduler")) != null) {
+    thisCalendarObj = JSON.parse(localStorage.getItem("WorkDayScheduler"));
+
+    $.each(thisCalendarObj, function (key, value) {
+      if (value != null) {
+        // console.log(key);
+        // console.log(value);
+        // console.log("[data-calendar-text='" + key + "']");
+        // console.log(`[data-calendar-text="` + key + `"]`);
+        var foundDataAttributeEl = $("input").find(
+          "[data-calendar-text='" + key + "']"
+        );
+        $("input")
+          .find("[data-calendar-text='" + key + "']")
+          .val("123123123");
+        //works
+        $("#9").val("1234asdf");
+
+        //doesn't work
+        if (i < 24) {
+          $(`"#` + i + `"`).val("1231asf");
+        }
+
+        // console.log(foundDataAttributeEl);
+        foundDataAttributeEl.val = "12323";
+        $("input").find(`[data-calendar-text="9"]`).text("123321123");
+        // $("input").val("12321");
+        $("#9").val(thisCalendarObj[9]);
+        $("#10").val(thisCalendarObj[10]);
+        $("#11").val(thisCalendarObj[11]);
+        $("#12").val(thisCalendarObj[12]);
+        $("#13").val(thisCalendarObj[13]);
+        $("#14").val(thisCalendarObj[14]);
+        $("#15").val(thisCalendarObj[15]);
+        $("#16").val(thisCalendarObj[16]);
+        $("#17").val(thisCalendarObj[17]);
+        $("#18").val(thisCalendarObj[18]);
+        $("#19").val(thisCalendarObj[19]);
+        $("#20").val(thisCalendarObj[20]);
+        $("#21").val(thisCalendarObj[21]);
+        $("#22").val(thisCalendarObj[22]);
+        $("#23").val(thisCalendarObj[23]);
+      }
+    });
+  }
 });
-
-//all of these buttons have a data element
-//find the data element on the page and add a button click event to them
-
-//with the button click, execute the function outside of the loop where we're building the element
-
-//nice to have - make onclick action on button which changes the
-//background color to light red when data is saved
-//to local storage
-
-//save text to local storage with the appropriate id
-
-//when you come back to the same page, load with text
-//in the appropriate text boxes from local storage
