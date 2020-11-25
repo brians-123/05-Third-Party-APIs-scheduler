c1a0d682311ba72cff3b31c1101cb69fd4636f10
brians-123 <brian_shor@hotmail.com>
2020-11-24 17:01:02 -0800

    cleaning up unused items and adding comments

--- scripts/script.js
+++ scripts/script.js
@@ -2,10 +2,9 @@
 var todaysDate = document.getElementById("currentDay");
 todaysDate.textContent = moment().format("dddd, MMM do, YYYY");
 
-//idea is to use military time to make comparisons easier
+//Use military time to make comparisons easier
 //each event area will have an id of the military time
 var momentHour = moment().format("H");
-// console.log(momentHour);
 
 var calendarContainer = $("#calendar-container");
 
@@ -15,7 +14,7 @@ for (var i = 9; i < 24; i++) {
   var rowTime = $("<div>").addClass(
     "text-right col-1 p-3 mr-2 text-nowrap border-top"
   );
-  // .attr("data-calendar-time", i);
+  //add the appropriate time to the left column
   var rowtimeText = i + " AM";
   if (i == 12) {
     rowtimeText = i + " PM";
@@ -23,11 +22,10 @@ for (var i = 9; i < 24; i++) {
   if (i > 12) {
     rowtimeText = i - 12 + " PM";
   }
-
   rowTime.text(rowtimeText);
+
   var rowSchedule = $("<input>")
     .addClass("w-100 p-3")
-    // .val("another calendar subject")
     .attr("data-calendar-text", i)
     .data("calendar-text", i)
     .attr("id", i);
@@ -46,6 +44,7 @@ for (var i = 9; i < 24; i++) {
     </svg>`);
   rowSaveButtonContainer.append(saveButtonImage);
 
+  //use the moment API to see if the event is in the past, the current hour, or in the future. color accordingly.
   if (i < momentHour) {
     rowSchedule.addClass("past");
   } else if (i == momentHour) {
@@ -64,22 +63,27 @@ var calendarData = [];
 //create an object for the calendar times and text
 var thisCalendarObj = {};
 
+//parse the values in objects back from local storage to render to the page
 if (JSON.parse(localStorage.getItem("WorkDayScheduler")) != null) {
   thisCalendarObj = JSON.parse(localStorage.getItem("WorkDayScheduler"));
-
-  $.each(thisCalendarObj, function (key, value) {
-    if (value != null) {
-      // console.log(key);
-      // console.log(value);
-      // console.log(`[data-calendar-text="` + key + `"]`);
-      $("input")
-        .find("[data-calendar-text='" + key + "']")
-        .val(value);
-      $("input").find(`[data-calendar-text="9"]`).val("123321123");
-    }
-  });
+  $("#9").val(thisCalendarObj[9]);
+  $("#10").val(thisCalendarObj[10]);
+  $("#11").val(thisCalendarObj[11]);
+  $("#12").val(thisCalendarObj[12]);
+  $("#13").val(thisCalendarObj[13]);
+  $("#14").val(thisCalendarObj[14]);
+  $("#15").val(thisCalendarObj[15]);
+  $("#16").val(thisCalendarObj[16]);
+  $("#17").val(thisCalendarObj[17]);
+  $("#18").val(thisCalendarObj[18]);
+  $("#19").val(thisCalendarObj[19]);
+  $("#20").val(thisCalendarObj[20]);
+  $("#21").val(thisCalendarObj[21]);
+  $("#22").val(thisCalendarObj[22]);
+  $("#23").val(thisCalendarObj[23]);
 }
 
+//the save button will save the new text for the current line into local storage
 $(".saveBtn").on("click", function saveInLocal() {
   var thisCalendarPosition = $(this).data("calendar-time");
 
@@ -88,49 +92,26 @@ $(".saveBtn").on("click", function saveInLocal() {
   thisCalendarObj[thisCalendarPosition] = thisCalendarText;
 
   localStorage.setItem("WorkDayScheduler", JSON.stringify(thisCalendarObj));
-  //trying to add in button with possible order of execution issue
-  if (JSON.parse(localStorage.getItem("WorkDayScheduler")) != null) {
+
+  if (JSON.parse(localStorage.getItem("WorkDayScheduler")) !== null) {
     thisCalendarObj = JSON.parse(localStorage.getItem("WorkDayScheduler"));
 
     $.each(thisCalendarObj, function (key, value) {
       if (value != null) {
-        // console.log(key);
-        // console.log(value);
-        // console.log("[data-calendar-text='" + key + "']");
-        // console.log(`[data-calendar-text="` + key + `"]`);
+        //Leaving this in for 2nd to last commit. I'd like to get help on the reason as to why this didn't work.
+        //Why couldn't I write to the value of an element using a variable in jquery? This was an attempt
+        //to look up the element by data attribute.
         var foundDataAttributeEl = $("input").find(
           "[data-calendar-text='" + key + "']"
         );
         $("input")
           .find("[data-calendar-text='" + key + "']")
           .val("123123123");
-        //works
-        $("#9").val("1234asdf");
 
-        //doesn't work
-        if (i < 24) {
-          $(`"#` + i + `"`).val("1231asf");
+        //loop through the key value pairs and display the appropriate value in the calendar
+        if (key < 24) {
+          $(`#` + key).val(thisCalendarObj[key]);
         }
-
-        // console.log(foundDataAttributeEl);
-        foundDataAttributeEl.val = "12323";
-        $("input").find(`[data-calendar-text="9"]`).text("123321123");
-        // $("input").val("12321");
-        $("#9").val(thisCalendarObj[9]);
-        $("#10").val(thisCalendarObj[10]);
-        $("#11").val(thisCalendarObj[11]);
-        $("#12").val(thisCalendarObj[12]);
-        $("#13").val(thisCalendarObj[13]);
-        $("#14").val(thisCalendarObj[14]);
-        $("#15").val(thisCalendarObj[15]);
-        $("#16").val(thisCalendarObj[16]);
-        $("#17").val(thisCalendarObj[17]);
-        $("#18").val(thisCalendarObj[18]);
-        $("#19").val(thisCalendarObj[19]);
-        $("#20").val(thisCalendarObj[20]);
-        $("#21").val(thisCalendarObj[21]);
-        $("#22").val(thisCalendarObj[22]);
-        $("#23").val(thisCalendarObj[23]);
       }
     });
   }
